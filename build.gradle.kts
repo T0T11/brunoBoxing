7ec616eb47a2717232b8ae0800d224b2b281d74e
T0T11 <tomas.orellano@gmail.com>
2026-04-23 10:13:12 +0200

    build(build.gradle): agregar dependencia

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.github.freva:ascii-table:1.8.0")
 }
+
+
 
 tasks.test {
     useJUnitPlatform()
