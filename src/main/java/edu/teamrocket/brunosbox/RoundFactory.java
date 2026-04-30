42b9f3c2fbfe4ea2378af09d65e1a27683f8da8b
ccastroto <castroaldrovercarlossangrinha@gmail.com>
2026-04-30 10:38:37 +0200

    feat(roundfactory): definir el case del factory para el calculo del loadJugde

--- src/main/java/edu/teamrocket/brunosbox/RoundFactory.java
+++ src/main/java/edu/teamrocket/brunosbox/RoundFactory.java
@@ -2,5 +2,13 @@ package edu.teamrocket.brunosbox;
 
 public class RoundFactory {
 
-
+    public static Round getRound(String roundScore) {
+        return switch (roundScore) {
+            case null -> null;
+            case "10 - 9", "9 - 10" -> new RegularRound(roundScore) ;
+            case "10 - 8", "8 - 10" -> new KnockdownRound(roundScore);
+            case "1, 8 - 10", "10 - 8 ,1" -> new PointsDeducted(roundScore);
+            default -> null;
+        };
+    }
 }
