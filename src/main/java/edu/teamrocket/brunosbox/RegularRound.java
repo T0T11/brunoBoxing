c36cae8eea815e479c111516135621a4f2a0d018
T0T11 <tomas.orellano@gmail.com>
2026-05-03 13:44:39 +0200

    refactor(RegularRound): refactorizacion de la clase

--- src/main/java/edu/teamrocket/brunosbox/RegularRound.java
+++ src/main/java/edu/teamrocket/brunosbox/RegularRound.java
@@ -3,36 +3,30 @@ package edu.teamrocket.brunosbox;
 public class RegularRound implements Round {
 
     private final String roundScore;
-    private byte redBoxerScore;
-    private byte blueBoxerScore;
+    public byte redBoxerScore;
+    public byte blueBoxerScore;
+    public RoundFactory[] roundfactory;
 
-    public RegularRound(String roundScore) {
-        this.roundScore = roundScore.replaceAll("\\s", "");
+    protected RegularRound(String roundScore){
+
+        this.roundScore = roundScore;
         boxerRoundScore();
     }
 
-    @Override
-    public byte getRedBoxerScore() {
-        return this.redBoxerScore;
-    }
-
-    @Override
-    public byte getBlueBoxerScore() {
-        return this.blueBoxerScore;
-    }
-
-    @Override
     public void boxerRoundScore() {
-        String[] scores = roundScore.split("-", 2);
-        this.redBoxerScore = Byte.parseByte(scores[0]);
-        this.blueBoxerScore = Byte.parseByte(scores[1]);
+
+        String[] resultado = roundScore.split("-");
+        redBoxerScore = Byte.parseByte(resultado[0]);
+        blueBoxerScore = Byte.parseByte(resultado[1]);
+
     }
 
     @Override
     public String toString() {
-        return getRedBoxerScore()+ " - " + getBlueBoxerScore();
+        return redBoxerScore + "-" + blueBoxerScore;
     }
+}
 
-    }
+
 
 
