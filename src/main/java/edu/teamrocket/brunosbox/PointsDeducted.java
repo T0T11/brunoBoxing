b63483648978b624e1017f1187035c0738a41a40
T0T11 <tomas.orellano@gmail.com>
2026-05-04 21:32:35 +0200

    refactor(PointDeducted): refactortizacion de la clase

--- src/main/java/edu/teamrocket/brunosbox/PointsDeducted.java
+++ src/main/java/edu/teamrocket/brunosbox/PointsDeducted.java
@@ -5,32 +5,54 @@ public class PointsDeducted implements Round{
     public byte redBoxerScore;
     public byte blueBoxerScore;
 
-    PointsDeducted(String roundScore) {
-        this.roundScore = roundScore;
-        this.redBoxerScore = redBoxerScore;
-        this.blueBoxerScore = blueBoxerScore;
+    protected PointsDeducted(String roundScore) {
+
+        this.roundScore = roundScore.replaceAll("\\s", "");
+        boxerRoundScore();
     }
 
-    @Override
+
+    String getRoundScore() {
+        return this.roundScore;
+    }
+
+        public void boxerRoundScore() {
+
+        String[] scores = getRoundScore().split("-", 2);
+        String redBoxerRoundScore = scores[0];
+        String blueBoxerRoundScore = scores[1];
+
+        if (redBoxerRoundScore.indexOf(',') == -1) {
+            this.redBoxerScore = Byte.parseByte(redBoxerRoundScore);
+            this.blueBoxerScore = this.parseComaBlue(blueBoxerRoundScore);
+        } else {
+            this.blueBoxerScore = Byte.parseByte(blueBoxerRoundScore);
+            this.redBoxerScore = this.parseComaRed(redBoxerRoundScore);
+        }
+    }
+
+    private Byte parseComaBlue(String score) {
+        // score = "8,1"
+        return Byte.parseByte(score.substring(0, score.indexOf(',')));
+    }
+
+    private Byte parseComaRed(String score) {
+        // score = "1,8"
+        return Byte.parseByte(score.substring(score.indexOf(',') + 1, score.length()));
+    }
+
+
     public byte getRedBoxerScore() {
         return this.redBoxerScore;
     }
 
-    @Override
+
     public byte getBlueBoxerScore() {
         return this.blueBoxerScore;
     }
 
     @Override
-    public void boxerRoundScore() {
-
-    }
-
-    private String parseComaBlue(String score){
-        return score;
-    }
-
-    private String parseComaRed(String score){
-        return score;
+    public String toString() {
+        return getRedBoxerScore() + " - " + getBlueBoxerScore();
     }
 }
